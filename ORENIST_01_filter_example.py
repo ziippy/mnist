a93a805ab8ffce6bf83a356314e1384ea34aa218
Jongsung-Kim <ziippy@naver.com>
2018-01-27 02:02:27 +0900

    ORENIST 관련 코드 등록

--- ORENIST_01_filter_example.py
+++ ORENIST_01_filter_example.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import _pickle as pickle
 
+# 데이터 파일 읽기
 with open('ORENIST.data', 'rb') as file:
     images, labels = pickle.load(file, encoding="bytes")
 
@@ -42,11 +43,48 @@ x = tf.placeholder(tf.float32, [None, 784])
 x_image = tf.reshape(x, [-1,28,28,1])
 
 W_conv = edge_filter()
-h_conv = tf.abs(tf.nn.conv2d(x_image, W_conv,
-                             strides=[1,1,1,1], padding='SAME'))
-h_conv_cutoff = tf.nn.relu(h_conv-0.2)
+h_conv = tf.abs(tf.nn.conv2d(x_image, W_conv,                       # 합성곱 필터를 적용하는 함수인 conv2d, 절대값을 취하는 함수인 abs
+                             strides=[1,1,1,1], padding='SAME'))    # SAME 은 존재하지 않는 부분의 픽셀은 0 으로 해서 계산
+h_conv_cutoff = tf.nn.relu(h_conv-0.2)      # 필터의 효과를 강조해서 알기 쉽게 하기 위해 추가
 
 h_pool =tf.nn.max_pool(h_conv_cutoff, ksize=[1,2,2,1],
                        strides=[1,2,2,1], padding='SAME')
 
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+filter_vals, conv_vals = sess.run([W_conv, h_conv_cutoff],
+                                  feed_dict={x:images[:9]})
+
+# 필터 적용한 데이터를 이미지화
+fig = plt.figure(figsize=(10, 3))
+
+for i in range(2):
+    subplot = fig.add_subplot(3, 10, 10 * (i + 1) + 1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(filter_vals[:, :, 0, i],
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+v_max = np.max(conv_vals)
+
+for i in range(9):
+    subplot = fig.add_subplot(3, 10, i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(images[i].reshape((28, 28)), vmin=0, vmax=1,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+    subplot = fig.add_subplot(3, 10, 10 + i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(conv_vals[i, :, :, 0], vmin=0, vmax=v_max,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+    subplot = fig.add_subplot(3, 10, 20 + i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(conv_vals[i, :, :, 1], vmin=0, vmax=v_max,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
