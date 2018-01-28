d696b2c858445c7aa5e27c88fec86a39397b8865
Jongsung-Kim <ziippy@naver.com>
2018-01-28 09:53:11 +0900

    .

---
+++ MNIST_03_cnn_classification.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 난수의 시드 생성
+np.random.seed(20180127)
+
+# MNIST 데이터 세트 다운로드
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+# 단층 신경망을 표현하는 클래스 정의
+class CnnNetwork:
+    def __init__(self, num_filters):
+        with tf.Graph().as_default():
+            self.prepare_model(num_filters)
+            self.prepare_session()
+
+    def prepare_model(self, num_filters):
+        with tf.name_scope('input'):
+            x = tf.placeholder(tf.float32, [None, 784], name='input')
+            x_image = tf.reshape(x, [-1,28,28,1])
+
+        with tf.name_scope('conv'):
+            w_conv = tf.Variable(tf.truncated_normal([5,5,1,num_filters], stddev=0.1))
+            h_conv = tf.nn.conv2d(x_image, w_conv, strides=[1,1,1,1], padding='SAME')
+            h_pool = tf.nn.max_pool(h_conv, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+        with tf.name_scope('fully-connection'):
+            h_pool_flat = tf.reshape(h_pool, [-1, 14*14*num_filters])
+
+            num_units1 = 14*14*num_filters
+            num_units2 = 1024
+
+            w2 = tf.Variable(tf.truncated_normal([num_units1, num_units2]))
+            b2 = tf.Variable(tf.zeros([num_units2]))
+            hidden2 = tf.nn.relu(tf.matmul(h_pool_flat, w2) + b2)
+
+        with tf.name_scope('output'):
+            w0 = tf.Variable(tf.zeros([num_units2, 10]), name='weights')
+            b0 = tf.Variable(tf.zeros([10]), name='biases')
+            p = tf.nn.softmax(tf.matmul(hidden2, w0) + b0, name='softmax')
+
+        with tf.name_scope('optimizer'):
+            t = tf.placeholder(tf.float32, [None, 10], name='labels')
+            loss = -tf.reduce_sum(t * tf.log(p), name='loss')
+            train_step = tf.train.AdamOptimizer(0.0005).minimize(loss)
+
+        with tf.name_scope('evaluator'):
+            correct_prediction = tf.equal(tf.argmax(p, 1), tf.argmax(t, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+
+        # 단일 값에 대해서는 summary.scalar
+        # 복수의 요소를 포함하는 것에 대해서는 summary.histogram
+        tf.summary.scalar("loss", loss)
+        tf.summary.scalar("accuracy", accuracy)
+        #tf.summary.histogram("weights_hidden", w1)
+        #tf.summary.histogram("biases_hidden", b1)
+        #tf.summary.histogram("weights_output", w0)
+        #tf.summary.histogram("biases_output", b0)
+
+        # 클래스 외부에서 참조할 필요가 있는 변수를 인스턴스 변수로 공개
+        self.x, self.t, self.p = x, t, p
+        self.train_step = train_step
+        self.loss = loss
+        self.accuracy = accuracy
+
+    def prepare_session(self):
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        #saver.restore(sess, './session/cnn_session-4000')
+        summary = tf.summary.merge_all()
+        writer = tf.summary.FileWriter("/tmp/mnist/03_cnn_logs", sess.graph)
+
+        self.sess = sess
+        self.summary = summary
+        self.writer = writer
+        self.saver = saver
+
+# 단층 CNN 인스턴스 생성 후 이를 사용
+nn = CnnNetwork(16)     # 16 is 필터 수
+
+# 파라미터 최적화를 4000회 반복
+i = 0
+for _ in range(4000):
+    i += 1
+    batch_xs, batch_ts = mnist.train.next_batch(100)        # 미니배치
+    #sess.run(train_step, feed_dict={x: batch_xs, t: batch_ts})
+    nn.sess.run(nn.train_step, feed_dict={nn.x: batch_xs, nn.t: batch_ts})
+    if i % 100 == 0:
+        #loss_val, acc_val = sess.run([loss, accuracy], feed_dict={x:mnist.test.images, t: mnist.test.labels})
+        summary, loss_val, acc_val = nn.sess.run([nn.summary, nn.loss, nn.accuracy], feed_dict={nn.x: mnist.test.images, nn.t: mnist.test.labels})
+        print ('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val))
+
+        nn.saver.save(nn.sess, './session/cnn_session', global_step=i)
+
+        # For, tensorboard
+        nn.writer.add_summary(summary, i)
+
+# 얻어진 결과를 실제 이미지로 확인
+images, labels = mnist.test.images, mnist.test.labels
+#p_val = sess.run(p, feed_dict={x:images, t: labels})
+p_val = nn.sess.run(nn.p, feed_dict={nn.x:images, nn.t: labels})
+
+fig = plt.figure(figsize=(8,15))
+for i in range(10):
+    c = 1
+    for (image, label, pred) in zip(images, labels, p_val):
+        prediction, actual = np.argmax(pred), np.argmax(label)
+        if prediction != i:
+            continue
+        if (c < 4 and i == actual) or (c >= 4 and i != actual):
+            subplot = fig.add_subplot(10,6,i*6+c)
+            subplot.set_xticks([])
+            subplot.set_yticks([])
+            subplot.set_title('%d / %d' % (prediction, actual))
+            subplot.imshow(image.reshape((28,28)), vmin=0, vmax=1,
+                           cmap=plt.cm.gray_r, interpolation="nearest")
+            c += 1
+            if c > 6:
+                break
+
+plt.show()
+
+###
+# Accuracy 결과는 약 0.981
+# 출력 계층의 softmax 만 사용했을 때의 정답률 (0.92) 에 비해서는 크게 개선되었다고 할 수 있다.
+# 신경망을 이용했을 때의 정답률 (0.97) 에 비해서 조금 개선되었다고 할 수 있다.
--- ORENIST_01_filter_example.py
+++ ORENIST_01_filter_example.py
@@ -42,7 +42,8 @@ def edge_filter():
 x = tf.placeholder(tf.float32, [None, 784])
 x_image = tf.reshape(x, [-1,28,28,1])
 
-W_conv = edge_filter()
+#W_conv = edge_filter()                                             # static filter
+W_conv = tf.Variable(tf.truncated_normal([5,5,1,2], stddev=0.1))    # dynamice filter
 h_conv = tf.abs(tf.nn.conv2d(x_image, W_conv,                       # 합성곱 필터를 적용하는 함수인 conv2d, 절대값을 취하는 함수인 abs
                              strides=[1,1,1,1], padding='SAME'))    # SAME 은 존재하지 않는 부분의 픽셀은 0 으로 해서 계산
 h_conv_cutoff = tf.nn.relu(h_conv-0.2)      # 필터의 효과를 강조해서 알기 쉽게 하기 위해 추가
@@ -87,4 +88,40 @@ for i in range(9):
     subplot.set_yticks([])
     subplot.imshow(conv_vals[i, :, :, 1], vmin=0, vmax=v_max,
                    cmap=plt.cm.gray_r, interpolation='nearest')
+
+# 풀링 계층
+pool_vals = sess.run(h_pool, feed_dict={x:images[:9]})
+
+# 풀링 계층 후 얻은 결과를 이미지로 출력
+fig = plt.figure(figsize=(10, 3))
+
+for i in range(2):
+    subplot = fig.add_subplot(3, 10, 10 * (i + 1) + 1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(filter_vals[:, :, 0, i],
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+v_max = np.max(pool_vals)
+
+for i in range(9):
+    subplot = fig.add_subplot(3, 10, i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(images[i].reshape((28, 28)), vmin=0, vmax=1,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+    subplot = fig.add_subplot(3, 10, 10 + i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(pool_vals[i, :, :, 0], vmin=0, vmax=v_max,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
+    subplot = fig.add_subplot(3, 10, 20 + i + 2)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(pool_vals[i, :, :, 1], vmin=0, vmax=v_max,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+
 plt.show()
